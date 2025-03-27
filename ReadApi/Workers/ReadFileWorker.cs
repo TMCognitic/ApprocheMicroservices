f64067ffdaac8c3ce7ce804e109ec44e3dc95162
Your Name <you@example.com>
2025-03-27 15:44:53 +0100

    init

---
+++ ReadApi/Workers/ReadFileWorker.cs
@@ -0,0 +1,25 @@
+﻿
+namespace ReadApi.Workers
+{
+    public class ReadFileWorker : BackgroundService
+    {
+        private readonly ILogger<ReadFileWorker> _logger;
+
+        public ReadFileWorker(ILogger<ReadFileWorker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (_logger.IsEnabled(LogLevel.Information))
+                {
+                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                }
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
